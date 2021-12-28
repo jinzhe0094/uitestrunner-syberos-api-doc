98e714e03004adffca639ec6788b8197f15c03c2
jinzhewang <wangjinzhe@syberos.com>
2021-12-28 14:01:27 +0800

    yyyy

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.32.4',
+    VERSION: '0.32.5',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
