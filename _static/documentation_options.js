2c5984167eb4b41e6a0e43e195431ace9e2708bb
jinzhe0094 <jinzhe0094@gmail.com>
2024-06-19 07:54:02 +0000

    2.4.12

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.4.11/html',
+    VERSION: '2.4.12/html',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
