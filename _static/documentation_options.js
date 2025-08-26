055257ae46cc15d73243646720e947ec25489ca2
jinzhe0094 <jinzhe0094@gmail.com>
2025-08-26 01:27:51 +0000

    2.12.0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.11.0/html',
+    VERSION: '2.12.0/html',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
