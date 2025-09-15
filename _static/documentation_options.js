294895aa1e7bc238f048d7d131c0c91df62aea5e
jinzhe0094 <jinzhe0094@gmail.com>
2025-09-15 07:56:53 +0000

    2.14.0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.13.1/html',
+    VERSION: '2.14.0/html',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
